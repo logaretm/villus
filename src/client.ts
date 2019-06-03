2a9796e1ea2a847ad2954680b9dc1ff1e81dbaad
Abdelrahman Awad <logaretm1@gmail.com>
2019-06-03 11:11:45 +0200

    refactor: mutate is not needed as its own method

--- src/client.ts
+++ src/client.ts
@@ -10,11 +10,6 @@ interface ClientQueryOptions {
   variables?: { [k: string]: any };
 }
 
-interface ClientMutationOptions {
-  mutation: string;
-  variables?: { [k: string]: any };
-}
-
 function resolveGlobalFetch (): Fetcher | undefined {
   // tslint:disable-next-line
   if (typeof window !== 'undefined' && 'fetch' in window) {
@@ -42,16 +37,6 @@ class VqlClient {
     return this.fetch(this.uri, {
       method: 'post',
       body: JSON.stringify({ query, variables }),
-      headers: {
-        'content-type': 'application/json'
-      }
-    });
-  }
-
-  mutate({ mutation, variables }: ClientMutationOptions) {
-    return this.fetch(this.uri, {
-      method: 'post',
-      body: JSON.stringify({ mutation, variables }),
       headers: {
         'content-type': 'application/json'
       }
