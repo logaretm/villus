b4bd290115929c35f2a71b651f8e09b0edd1d9c2
Abdelrahman Awad <logaretm1@gmail.com>
2020-01-07 13:16:15 +0200

    test: make the mount helper work with vue 3

--- test/helpers/mount.ts
+++ test/helpers/mount.ts
@@ -1,10 +1,22 @@
-// @ts-ignore
-import * as stuff from 'vue/dist/vue.global.js';
+import { createApp, compile } from 'vue';
 
 export function mount(component: Record<string, any>) {
-  const app = stuff.createApp();
+  const app = createApp();
   app.config.devtools = false;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  app.config.warnHandler = () => {};
+  app.config.errorHandler = err => {
+    if (err.message === 'data is not defined') {
+      return;
+    }
+
+    // eslint-disable-next-line no-console
+    console.error(err);
+  };
+
   document.body.innerHTML = `<div id="app"></div>`;
+  component.render = compile(component.template);
+  component.template = undefined;
 
   return app.mount(component, '#app');
 }
