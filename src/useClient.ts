3db58d5518a05776e33a7d541f70e005e6b0a2fb
Abdelrahman Awad <logaretm1@gmail.com>
2020-02-21 15:09:32 +0200

    feat: useClient should return the client instance

--- src/useClient.ts
+++ src/useClient.ts
@@ -4,4 +4,6 @@ import { createClient, VqlClientOptions, VqlClient } from './client';
 export function useClient(opts: VqlClientOptions | VqlClient) {
   const client = opts instanceof VqlClient ? opts : createClient(opts);
   provide('$villus', client);
+
+  return client;
 }
