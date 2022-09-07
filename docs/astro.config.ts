7af6ee3629cad3a0b6a4f611c01362d76d657186
Liam Stanley <me@liamstanley.io>
2022-09-07 00:25:46 -0400

    docs: fix markdown tables
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import highlight from './highlight';
+import { defineConfig } from "astro/config"
+import remarkGfm from "remark-gfm"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import vue from "@astrojs/vue"
+import highlight from "./highlight"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
     vue(),
     sitemap(),
     mdx({
-      remarkPlugins: [highlight],
+      remarkPlugins: [remarkGfm, highlight],
     }),
   ],
 });
