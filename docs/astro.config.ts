85d40af75faa51e8ad636079fb923f4896245566
Abdelrahman Awad <logaretm1@gmail.com>
2023-08-20 17:57:47 +0300

    style: formatting

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config"
-import remarkGfm from "remark-gfm"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import vue from "@astrojs/vue"
-import highlight from "./highlight"
+import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
+import highlight from './highlight';
 
 // https://astro.build/config
 export default defineConfig({
